21b5ed320b2d053d640ffb9d6a85063a94d04d6a
Martho Evan <evanom59@gmail.com>
2018-05-02 22:15:56 +0200

    :sparkles: Create TodoList

---
+++ src/app/main/content/todolist/todo-list-item/todo-list-item.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'app-todo-list-item',
+    templateUrl: './todo-list-item.component.html',
+    styleUrls: ['./todo-list-item.component.scss'],
+})
+export class TodoListItemComponent implements OnInit {
+    constructor() {}
+
+    ngOnInit() {}
+}
---
+++ src/app/main/content/todolist/todolist.module.ts
@@ -0,0 +1,9 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { TodolistComponent } from './todolist.component';
+
+@NgModule({
+    imports: [CommonModule],
+    declarations: [TodolistComponent],
+})
+export class TodolistModule {}
