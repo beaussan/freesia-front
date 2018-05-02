44f122931316db6d5f42a763ba7609b1a68c65f9
Nicolas Beaussart <nic.beaussart@gmail.com>
2018-05-02 22:27:32 +0200

    :sparkles: todo ugly

--- src/app/main/content/todolist/todolist.module.ts
+++ src/app/main/content/todolist/todolist.module.ts
@@ -1,9 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FuseSharedModule } from '@fuse/shared.module';
+import { RouterModule } from '@angular/router';
 import { TodolistComponent } from './todolist.component';
+import { TodoListItemComponent } from './todo-list-item/todo-list-item.component';
+
+const routes = [
+    {
+        path: 'todolists',
+        component: TodolistComponent,
+    },
+];
 
 @NgModule({
-    imports: [CommonModule],
-    declarations: [TodolistComponent],
+    imports: [RouterModule.forChild(routes), FuseSharedModule, CommonModule],
+    declarations: [TodolistComponent, TodoListItemComponent],
 })
 export class TodolistModule {}
